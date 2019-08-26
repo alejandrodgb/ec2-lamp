7038e14b11cf3956a59b9a328bf43bdf3ba903b2
Alejandro DGB <alex@scitaanalytics.com.au>
2019-08-26 17:48:34 +1000

    Add line to test ansible pull

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -5,5 +5,6 @@
     </head>
     <body>
         <h1><?php echo 'Welcome to DevOps on AWS. This text was generated using PHP7' ?></h1>
+        <h2><?php echo 'This change will be reflected in the ansible pull command'?></h2>
     </body>
 </html>
